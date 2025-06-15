84906de6a588e85b867650ada4182ed93cee277c
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2025-06-15 22:18:16 +0100

    chore: terraform fmt

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  ami_id                     = "ami-0eb260c4d5475b901" # Amazon Linux 2 AMI in eu-west-2 (London)
-  availability_zones         = ["a", "b"]
+  ami_id             = "ami-0eb260c4d5475b901" # Amazon Linux 2 AMI in eu-west-2 (London)
+  availability_zones = ["a", "b"]
 }
